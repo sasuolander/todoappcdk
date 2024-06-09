f7bb0594cf46bffd4f278e04a1ce72598e9cf2c5
Sasu <sasuolander@yahoo.com>
2024-06-09 16:16:19 +0300

    Initial commit

---
+++ test/resttodoapp.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Resttodoapp from '../lib/resttodoapp-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/resttodoapp-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Resttodoapp.ResttodoappStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
